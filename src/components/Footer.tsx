8342a1d8710288ba5bb6fd65e32cb77075214529
ivory <contact@ivory.cafe>
2025-10-22 14:17:40 +0800

    fix github url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,11 +14,11 @@ const Footer: React.FC<FooterProps> = ({ onOpenAdvancedSettings }) => {
         </div>
         <div className="copyright-text">
           💻 <a
-            href="https://github.com/tiwe0/cute-frpc-tauri"
+            href="https://github.com/tiwe0/bluelotus"
             target="_blank"
             rel="noopener noreferrer"
           >
-            https://github.com/tiwe0/cute-frpc-tauri
+            https://github.com/tiwe0/bluelotus
           </a>
         </div>
         <div
