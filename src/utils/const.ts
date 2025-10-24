d47913661c4c655d5c0ea6aa03458d5496daa641
ivory <contact@ivory.cafe>
2025-10-24 19:21:59 +0800

    fix: 修复初始化问题，并在windows上通过测试

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,8 +1,14 @@
+import { exists, mkdir } from "@tauri-apps/plugin-fs";
 import { resolveResource, appConfigDir } from "@tauri-apps/api/path";
 
+export const APP_CONFIG_DIR = await appConfigDir();
 export const APP_DEFAULT_FRPC_CONFIG_PATH = await resolveResource("resources/default_config.toml");
 export const APP_FRPC_CONFIG_PATH = `${await appConfigDir()}/frpc_config.toml`;
 export const APP_SAKURA_API_KEY_PATH = `${await appConfigDir()}/sakura_api_key.txt`;
+
+if (await exists(APP_CONFIG_DIR) === false) {
+  await mkdir(APP_CONFIG_DIR);
+}
 
 export default {
   APP_DEFAULT_FRPC_CONFIG_PATH,
